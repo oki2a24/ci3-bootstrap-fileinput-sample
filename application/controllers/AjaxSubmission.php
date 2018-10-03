a223b285fdcebc94bcd6c893f4cc052174b96b85
oki2a24 <oki2a24@gmail.com>
2018-10-04 08:12:22 +0900

    style: クラス内でのメソッドの順番を入れ替える

--- application/controllers/AjaxSubmission.php
+++ application/controllers/AjaxSubmission.php
@@ -8,27 +8,45 @@ class AjaxSubmission extends CI_Controller {
 	}
 
 	/**
-	 * ファイルを削除します。
-	 * ファイルは複数ではなく、1つのみ受け取ります。
+	 * ページ初期表示時
 	 *
 	 * @return void
 	 */
-	public function delete()
+	public function index()
 	{
-		$posted = $this->input->post();
-		// ファイル削除
-		$is_deleted = unlink(self::UPLOAD_PATH . $posted['key']);
+		// サーバのファイル情報を取得
+		$this->load->helper('file');
+		$dir_file_info = get_dir_file_info(self::UPLOAD_PATH);
 
-		// 結果のレスポンス
-		if (!$is_deleted) {
-			$this->output->set_content_type('application/json', 'utf-8')
-			->set_output(json_encode(array(
-				'error' => '削除できませんでした。',
-			)));
-			return;
+		// HTML に渡す initialPreview オプション、initialPreviewConfig オプションデータ生成
+		$initial_preview_json_array = [];
+		$initial_preview_config_json_array = [];
+		foreach ($dir_file_info as $file_name => $v) {
+			$initial_preview_json_array[] = sprintf('%1$s%2$s%3$s', base_url(), self::UPLOAD_PATH, $file_name);
+
+			$mime_by_extension = get_mime_by_extension($file_name);
+			$initial_preview_config_json_array[] = [
+				'filetype' => $mime_by_extension,
+				'caption' => $file_name,
+				'size' => $v['size'],
+				'url' => sprintf('%1$s/ajaxsubmission/delete', site_url()),
+				'key' => $file_name, 
+				'extra' => [
+					'target_file_name' => $file_name
+				]
+			];
 		}
-		$this->output->set_content_type('application/json', 'utf-8')
-		->set_output('{}');
+		$initial_preview_json = json_encode($initial_preview_json_array, JSON_UNESCAPED_SLASHES);
+		$initial_preview_config_json = json_encode($initial_preview_config_json_array, JSON_UNESCAPED_SLASHES);
+
+		// HTML へ渡すデータ組み立て、レスポンス
+		$data = [
+			'fileinput_init' => [
+				'initialPreview' => 'initialPreview: ' . $initial_preview_json,
+				'initialPreviewConfig' => 'initialPreviewConfig: ' . $initial_preview_config_json,
+			],
+		];
+		$this->load->view('ajaxsubmission/index', $data);
 	}
 
 	/**
@@ -89,44 +107,26 @@ class AjaxSubmission extends CI_Controller {
 	}
 
 	/**
-	 * ページ初期表示時
+	 * ファイルを削除します。
+	 * ファイルは複数ではなく、1つのみ受け取ります。
 	 *
 	 * @return void
 	 */
-	public function index()
+	public function delete()
 	{
-		// サーバのファイル情報を取得
-		$this->load->helper('file');
-		$dir_file_info = get_dir_file_info(self::UPLOAD_PATH);
+		$posted = $this->input->post();
+		// ファイル削除
+		$is_deleted = unlink(self::UPLOAD_PATH . $posted['key']);
 
-		// HTML に渡す initialPreview オプション、initialPreviewConfig オプションデータ生成
-		$initial_preview_json_array = [];
-		$initial_preview_config_json_array = [];
-		foreach ($dir_file_info as $file_name => $v) {
-			$initial_preview_json_array[] = sprintf('%1$s%2$s%3$s', base_url(), self::UPLOAD_PATH, $file_name);
-
-			$mime_by_extension = get_mime_by_extension($file_name);
-			$initial_preview_config_json_array[] = [
-				'filetype' => $mime_by_extension,
-				'caption' => $file_name,
-				'size' => $v['size'],
-				'url' => sprintf('%1$s/ajaxsubmission/delete', site_url()),
-				'key' => $file_name, 
-				'extra' => [
-					'target_file_name' => $file_name
-				]
-			];
+		// 結果のレスポンス
+		if (!$is_deleted) {
+			$this->output->set_content_type('application/json', 'utf-8')
+			->set_output(json_encode(array(
+				'error' => '削除できませんでした。',
+			)));
+			return;
 		}
-		$initial_preview_json = json_encode($initial_preview_json_array, JSON_UNESCAPED_SLASHES);
-		$initial_preview_config_json = json_encode($initial_preview_config_json_array, JSON_UNESCAPED_SLASHES);
-
-		// HTML へ渡すデータ組み立て、レスポンス
-		$data = [
-			'fileinput_init' => [
-				'initialPreview' => 'initialPreview: ' . $initial_preview_json,
-				'initialPreviewConfig' => 'initialPreviewConfig: ' . $initial_preview_config_json,
-			],
-		];
-		$this->load->view('ajaxsubmission/index', $data);
+		$this->output->set_content_type('application/json', 'utf-8')
+		->set_output('{}');
 	}
 }
